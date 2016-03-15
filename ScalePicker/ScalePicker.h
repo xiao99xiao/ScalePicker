ba1ed8ffa9e91a1e6930d06a6bed1742d1d9ce0f
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2016-03-15 21:23:43 +0800

    Added Carthage support

---
+++ ScalePicker/ScalePicker.h
@@ -0,0 +1,19 @@
+//
+//  ScalePicker.h
+//  ScalePicker
+//
+//  Created by Dmitry on 15/3/16.
+//  Copyright © 2016 Dmitry Klimkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScalePicker.
+FOUNDATION_EXPORT double ScalePickerVersionNumber;
+
+//! Project version string for ScalePicker.
+FOUNDATION_EXPORT const unsigned char ScalePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScalePicker/PublicHeader.h>
+
+
