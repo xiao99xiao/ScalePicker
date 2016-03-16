c5932977d5d11e01c957dc84a335c0eefe385694
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2016-03-16 10:14:09 +0800

    Updated framework header file

--- ScalePicker/ScalePicker.h
+++ ScalePicker/ScalePicker.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Dmitry Klimkin. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for ScalePicker.
 FOUNDATION_EXPORT double ScalePickerVersionNumber;
