3a19c506ca2c8ba10c01b3dcba5026f9826a26b8
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2016-06-02 16:30:33 +0800

    Fixed warning

--- ScalePicker/ScalePicker.swift
+++ ScalePicker/ScalePicker.swift
@@ -443,7 +443,6 @@ public class ScalePicker: UIView, SlidePickerDelegate {
         let minScale: CGFloat    = 0.0
         let maxScale: CGFloat    = 0.2
         let maxOffset: CGFloat   = 50.0
-        let minOffset: CGFloat   = 0.0
         var offsetShift: CGFloat = 0.0
         var scaleShift: CGFloat  = 1.0
         var offsetValue          = offset
